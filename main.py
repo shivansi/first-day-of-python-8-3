5c1d9448e1bc0e8d2589966537c2eb6dc99c55aa
S B <barat.shivansi@gmail.com>
2020-08-03 20:52:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# print("hello world")
+
+# #variable
+# #ex: score and health in games.
+
+# #name of variable=value
+# score=0
+# name="spongebob"
+# health=100
+
+# print(name)
+
+# #conditionals - if/else statements used for decision making
+
+# if score== 5: 
+#   print("you have 5 score")
+# else:
+#   print("score is 0")
+
+# num=30
+
+# #create an if/else statement that will print.
+# #whether num is less than or greater than 30.
+
+# if num > 30:
+#   print("num is greater than 30")
+# elif num==30:
+#   print(num equals 30)
+# else:
+#   print("num is less than 30")
+
+# #loops
+
+# for i in range(5):
+#   print("smelly cat")
+# count=0
+# while(count<9):
+#   print("the count is :"+str(count))
+#   count+=1
+
+###########################
+
+name=input("what is your name?")
+print("Hello"+name)
+
+#list
+avengers=["balck widow", "iron man"]
+#var
+avengers_count=4
+
+print("hello")
+fullName=input("what is your first and last name?")
+age=input("how old r u?")
+activity=input("what is your favorite activity?")
+print("your name is"+fullName)
+print("you are"+ age)
+print("your favorite activity is"+activity)
+
+num1=int(input("put one number here!"))
+num2=int(input("put another number here!"))
+
+print("the sum is:{}".format(num1+num2))
+
